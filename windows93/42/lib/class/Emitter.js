e7ae35f55ced8c80b73c0c56cd3a0e48f1b9a5ed
epickfr <jacob.daniel.morales@outlook.com>
2026-04-11 20:14:47 -0500

    Add Windows93 system files and project updates

---
+++ windows93/42/lib/class/Emitter.js
@@ -0,0 +1,4 @@
+import {
+  Emittable
+} from "./mixin/Emittable.js"
+export const Emitter = Emittable()
---
+++ windows93/42/lib/class/error/AbortError.js
@@ -0,0 +1,13 @@
+export class AbortError extends Error {
+  constructor(message) {
+    super(message)
+    Object.defineProperties(this, {
+      name: {
+        value: "AbortError"
+      },
+      code: {
+        value: DOMException.ABORT_ERR
+      },
+    })
+  }
+}
